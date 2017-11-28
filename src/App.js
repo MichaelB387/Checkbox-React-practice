04f38d7514ebec8e2897e4139e1d91e53e21b0f8
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2017-11-28 12:11:43 +1100

    Change to use datat to render todo items

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/Todoitem'
 
+// Data Model
+const items =[
+  { description: 'First', completed: true},
+  { description: 'Second', completed: true},
+  { description: 'Third', completed: false}, 
+]
+
+
+// UI (view)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoItem description='First' completed/>
-        <TodoItem description='Second' completed/>
-        <TodoItem description='Third'/>
+      {
+        items.map((item, index) => (
+          <TodoItem
+          description={ item.description}
+          completed={item.completed}
+          />
+        )) 
+      }
       </div>
     );
   }
